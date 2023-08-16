d2cc7d9c8d2a1d3532e486397aa9ad7908492cd8
Yana Shimanskaya <yana-shimanskaya@mail.ru>
2023-08-16 11:28:08 +0300

    changes commit

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CatTest {
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSound() {
+        Cat cat = new Cat(feline);
+        String expected = "Мяу";
+        String actual = cat.getSound();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFood() throws Exception {
+        Cat cat = new Cat(feline);
+        cat.getFood();
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+    }
+
 }
--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class FelineTest {
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class FelineTest {
+    private final int expected;
+    private final int expKittensCount = 1;
+    Feline feline;
+
+    public FelineTest(int kittensCount) {
+        this.expected = kittensCount;
+    }
+
+    @Parameterized.Parameters(name = "Kittens count. Test data: {0}")
+    public static Object[][] kittenCount() {
+        return new Object[][]{
+                {0},
+                {1},
+                {5},
+
+        };
+    }
+
+    @Before
+    public void feline() {
+        feline = new Feline();
+    }
+
+    @Test
+    public void eatMeat() throws Exception {
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
+    }
+
+    @Test
+    public void getFamily() {
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensWoParams() {
+        assertEquals(expKittensCount, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensWithParams() {
+        int actual = feline.getKittens(expected);
+        assertEquals(expected, actual);
+    }
 }
+
